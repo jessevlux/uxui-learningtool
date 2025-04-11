d132a66d853fe212a140ccea546585c9bf444126
Jesse van Luxemburg <jessevanluxemburg@gmail.com>
2025-04-11 16:28:36 +0200

    error fix

--- src/app/lesson/[moduleId]/[lessonId]/page.tsx
+++ src/app/lesson/[moduleId]/[lessonId]/page.tsx
@@ -2,11 +2,15 @@ import LessonContent from "@/components/LessonContent";
 import { FaArrowLeft } from "react-icons/fa";
 import ClientLessonPage from "./ClientLessonPage";
 
+interface PageProps {
+  params: Promise<{
+    moduleId: string;
+    lessonId: string;
+  }>;
+}
+
 // Server Component
-export default function LessonPage({
-  params,
-}: {
-  params: { moduleId: string; lessonId: string };
-}) {
-  return <ClientLessonPage formattedLessonId={params.lessonId} />;
+export default async function LessonPage({ params }: PageProps) {
+  const resolvedParams = await params;
+  return <ClientLessonPage formattedLessonId={resolvedParams.lessonId} />;
 }
